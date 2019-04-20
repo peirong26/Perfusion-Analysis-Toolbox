8e9ba29310f7ee50eeefcf6de1e7f60040096767
Peirong26 <44064021+Peirong26@users.noreply.github.com>
2019-04-20 12:22:48 -0400

    Add files via upload

---
+++ ParamsCalculator/mask.py
@@ -0,0 +1,17 @@
+import os
+import torch
+import logging
+import numpy as np
+from builtins import object
+
+# Brain mask computation
+
+
+def cal(raw_perf, device):
+
+    print('Masking raw data ...')
+
+    a
+
+    return mask
+
